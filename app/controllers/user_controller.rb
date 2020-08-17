497f09e08c8a98434d983e6070839191080dd541
Benny Hung <wakenightryu@gmail.com>
2020-08-17 02:19:02 -0400

    used rails g controller to generate user, song, cover controllers

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+  
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'cover/index'
+  get 'cover/show'
+  get 'cover/new'
+  get 'cover/edit'
+  get 'song/index'
+  get 'song/show'
+  get 'song/new'
+  get 'song/edit'
+  get 'user/new'
+  get 'user/show'
+  get 'user/edit'
+  get 'user/song'
+  get 'user/cover'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
